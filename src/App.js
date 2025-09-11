f770149dece06fedf1b4daa9012accea53298b69
sandeep parmar <23bit062@ietdavv.edu.in>
2025-09-11 14:27:47 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function AppContent() {
     return (
       <div className="flex items-center justify-center min-h-screen">
         <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-ocean-medium"></div>
+        
       </div>
     );
   }
